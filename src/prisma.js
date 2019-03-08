deee409de9c808b7e3dded469bb5b5bc343f185d
kmlee95 <kmlee95@naver.com>
2019-03-08 23:53:05 +0900

    prisma 변경

--- src/prisma.js
+++ src/prisma.js
@@ -2,7 +2,7 @@ import { Prisma } from "prisma-binding";
 
 const prisma = new Prisma({
 	typeDefs: "src/generated/prisma.graphql",
-	endpoint: "http://localhost:4466"
+	endpoint: "https://us1.prisma.sh/kyungminlee-5dfb92/remody-server/dev"
 });
 
 export { prisma as default };
